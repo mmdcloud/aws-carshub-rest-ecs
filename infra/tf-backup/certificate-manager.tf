c80ca6d2062d3a24d5a99e7953af01762466e9ee
mmdcloud <madmaxcloudpractice@gmail.com>
2025-03-29 09:58:31 +0530

    update: removed tf-backup

--- infra/tf-backup/certificate-manager.tf
+++
@@ -1,9 +0,0 @@
-# resource "aws_acm_certificate" "carshub_cert" {
-#   domain_name       = "madmaxcloud.online"  
-#   validation_method = "DNS"
-# }
-
-# resource "aws_acm_certificate_validation" "carshub_cert_validate" {
-#   certificate_arn         = aws_acm_certificate.carshub_cert.arn
-#   validation_record_fqdns = [for record in aws_route53_record.aws_route53_records : record.fqdn]
-# }
